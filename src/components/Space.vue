c77e81c0f0baf4cf3bad0874711000761856e2a2
kaue.curti <kauecurti@hotmail.com>
2022-05-04 10:12:01 -0300

    add prod

--- src/components/Space.vue
+++ src/components/Space.vue
@@ -5,6 +5,9 @@
       <div class="options">
         <select id="planets" v-model="planetColor" aria-label="Planets">
           <option value="#ff5f40">teste prod</option>
+          <option value="#ff5f40">teste prod</option>
+          <option value="#ff5f40">teste prod</option>
+          <option value="#ff5f40">teste prod</option>
         </select>
       </div>
 
